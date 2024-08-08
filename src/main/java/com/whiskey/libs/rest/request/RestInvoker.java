326a926da8c93c3701ae0529acf4f8d3a30efc7f
Awesome <awesome_devnet@outlook.com>
2024-08-08 10:01:26 +0900

    update v1.0.3

--- src/main/java/com/whiskey/libs/rest/request/RestInvoker.java
+++ src/main/java/com/whiskey/libs/rest/request/RestInvoker.java
@@ -69,7 +69,7 @@ public class RestInvoker<T> {
                 return gson.fromJson(response, responseType);
             } else {
                 String errorResponse = readResponse(connection.getErrorStream());
-                throw new RuntimeException("Failed : HTTP error code : " + code + ", Response: " + errorResponse + ", ERROR MESSAGE: " + connection.getErrorStream());
+                throw new RuntimeException("Failed : HTTP error code : " + code + ", Response: " + errorResponse + ", ERROR MESSAGE: " + readResponse(connection.getErrorStream()));
             }
         } finally {
             connection.disconnect(); // 연결 해제
